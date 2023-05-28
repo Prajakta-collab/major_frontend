13f8ea9c8632386694f053c713f3c4dc0ab55374
Prajakta-collab <prajaktabadgujar2281@gmail.com>
2023-05-29 00:53:53 +0530

    upload file axios call at frontend

--- src/components/Attendant/HomeAtt.js
+++ src/components/Attendant/HomeAtt.js
@@ -5,6 +5,7 @@ import { Link, useLocation, useHistory } from "react-router-dom";
 import creditContext from '../../context/credits/creditContext';
 import AttSidebar from '../Sidebar/AttSidebar';
 import Swal from 'sweetalert2'
+import axios from 'axios'
 
 
 const HomeAtt = () => {
@@ -12,9 +13,28 @@ const HomeAtt = () => {
     
 
     const context = useContext(creditContext);
-    const { credit, searchByvno,request, getrequest, completerequest,cardpump,getcardpumpat ,toggle,handleToggle} = context;
+    const {  searchByvno,request, getrequest, completerequest,cardpump,getcardpumpat ,handleToggle} = context;
 
     const [vehicleNo,setVehicleNo]=useState("")
+    
+  const [file, setfile] = useState({selectedfile:null})
+  const onChangeHandler=(e)=>{
+setfile({selectedfile:e.target.files[0]})
+  }
+  const onScanClick = () => {
+    const data = new FormData();
+    data.append("file", this.state.selectedFile);
+    
+    axios
+    .post("http://localhost:5001/api/fuel/uploadqr", data)
+    .then(res => {
+       console.log("res.statusText",res.statusText);
+       
+    })
+    .catch(err => {
+       console.log(err);
+    });
+ };
 
     useEffect(() => {
         getrequest();
@@ -31,8 +51,7 @@ const HomeAtt = () => {
         setVehicleNo(e.target.value);
     }
 
-
-
+   
    
     return (
 
@@ -141,7 +160,8 @@ const HomeAtt = () => {
                            
                             </div>
                             <div>
-                                <button><i className="fas fa-qrcode"></i></button>
+                            <input type="file" onChange={onChangeHandler} />
+                                <button onClick={onScanClick}><i className="fas fa-qrcode"></i></button>
                             </div>
 
 
